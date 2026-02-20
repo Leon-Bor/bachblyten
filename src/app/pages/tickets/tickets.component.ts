668ea544cde13212e988ffa79a9b4130e8591bd1
Leon Borlinghaus <lborlinghaus@gamomat.com>
2026-02-20 09:34:59 +0100

    Fix(tickets): Updates urgency copy
    Updates the urgency copy to reflect a more accurate representation of the sold percentage of Early-Bird-Tickets.

--- src/app/pages/tickets/tickets.component.ts
+++ src/app/pages/tickets/tickets.component.ts
@@ -25,7 +25,7 @@ export class TicketsComponent {
     return `Nur noch ${this.remainingPercentage}% übrig`;
   }
   get urgencyCopy() {
-    return `Fast ${this.soldPercentage}% der Early-Bird-Tickets weg. Die nächste Preisstufe wird teurer – Dat is ’n Schnapper, sach ich dir. Seh to!`;
+    return `Über ${this.soldPercentage}% der Early-Bird-Tickets weg. Die nächste Preisstufe wird teurer – Dat is ’n Schnapper, sach ich dir. Seh to!`;
   }
 
   protected readonly paylogicUrl =
