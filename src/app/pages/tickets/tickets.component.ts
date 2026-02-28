155934dbcce1b643d96b58225dea52c4f0f5ccdc
Leon Borlinghaus <lborlinghaus@gamomat.com>
2026-02-28 11:04:54 +0100

    fix(tickets): Increase desktop iframe height
    Increments the base height for the iframe by 10 pixels on desktop views to improve content display and prevent potential cutoff issues.

--- src/app/pages/tickets/tickets.component.ts
+++ src/app/pages/tickets/tickets.component.ts
@@ -39,7 +39,7 @@ export class TicketsComponent {
   onIframeLoad() {
     // is mobile?
     const isMobile = window.innerWidth <= 768;
-    const baseHeight = isMobile ? 1850 : 1350;
+    const baseHeight = isMobile ? 1850 : 1360;
 
     this.iframeHeight = baseHeight;
   }
